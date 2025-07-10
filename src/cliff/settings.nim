442fd7ae44fcc5b165ca3698b71693ce9ed6f352
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2025-07-10 04:47:00 -0400

    improvements to settings

--- src/cliff/settings.nim
+++ src/cliff/settings.nim
@@ -2,27 +2,31 @@ import std/options
 import fields
 
 type
+    CSFieldOrderElement* {.size:sizeof(byte).} = enum
+        order_ID   = 0, ## - identifier of the chunk type
+        order_VER  = 1, ## - version of the chunk type. can be useful for files which are themselves one chunk, that starts with a file wide version
+        order_LEN1 = 2, ## - length 1 of data
+        order_LEN2 = 3, ## - override length of data
+        order_PAL  = 4, ## - calculates lengths of prepend and append
+        order_field_len = 5
+
     CliffSettings* = object
-        ## remaps the following list to set field processing order
-        ## [
-        ## ID   - identifier of the chunk type
-        ## VER  - version of the chunk type. can be useful for files which are themselves one chunk, that starts with a file wide version
-        ## LEN1 - length 1 of data
-        ## LEN2 - override length of data
-        ## ]
-        field_order *: ByteMapping
+        ## sets field processing order
+        ## ordering must start with ID, LEN1, or VER
+        field_order *: array[order_field_len, CSFieldOrderElement]
 
         ## fields, containing values and types, are passed in in `field_order` order, as the values are parsed
         ## the first proc gets no input, second gets one, third gets two, and fourth gets three
         ## crc gets all values
+        ## if a field is not returned by a preceeding proc, it will be `none()` in `fields`
         ##
         ## once `prepend_append_lengths` is called
         ## all following procs will recieve a value for either the prepend (first three) or append (crc)
-        field_id   *: proc (spf: seq[PositionedField], pal: Option[int]): PositionedField
-        field_ver  *: proc (spf: seq[PositionedField], pal: Option[int]): Option[PositionedField]
-        field_len1 *: proc (spf: seq[PositionedField], pal: Option[int]): PositionedField
-        field_len2 *: proc (spf: seq[PositionedField], pal: Option[int]): Option[PositionedField]
-        prepend_append_lengths *: proc (spf: seq[PositionedField]): (int, int)
+        field_id   *: proc (fields: seq[Option[Field]], pal: Option[int]): PositionedField
+        field_ver  *: proc (fields: seq[Option[Field]], pal: Option[int]): Option[PositionedField]
+        field_len1 *: proc (fields: seq[Option[Field]], pal: Option[int]): PositionedField
+        field_len2 *: proc (fields: seq[Option[Field]], pal: Option[int]): Option[PositionedField]
+        prepend_append_lengths *: proc (fields: seq[Option[Field]]): (int, int)
         ## always determined last
-        field_crc  *: proc (spf: seq[PositionedField], pal: Option[int]): Option[PositionedField]
-        crc_after  *: proc (spf: seq[PositionedField], pal: Option[int]): bool
+        field_crc  *: proc (fields: seq[Option[Field]], pal: Option[int]): Option[PositionedField]
+        crc_after  *: proc (fields: seq[Option[Field]], pal: Option[int]): bool
