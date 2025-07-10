9a8f34cfcf442f5dc2dcd872119730c109c7f84f
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2025-07-10 04:10:37 -0400

    option instead of use bool proc

--- src/cliff/settings.nim
+++ src/cliff/settings.nim
@@ -19,13 +19,10 @@ type
         ## once `prepend_append_lengths` is called
         ## all following procs will recieve a value for either the prepend (first three) or append (crc)
         field_id   *: proc (spf: seq[PositionedField], pal: Option[int]): PositionedField
-        field_ver  *: proc (spf: seq[PositionedField], pal: Option[int]): PositionedField
-        use_ver    *: proc (spf: seq[PositionedField], pal: Option[int]): bool
+        field_ver  *: proc (spf: seq[PositionedField], pal: Option[int]): Option[PositionedField]
         field_len1 *: proc (spf: seq[PositionedField], pal: Option[int]): PositionedField
-        field_len2 *: proc (spf: seq[PositionedField], pal: Option[int]): PositionedField
-        use_len2   *: proc (spf: seq[PositionedField], pal: Option[int]): bool
+        field_len2 *: proc (spf: seq[PositionedField], pal: Option[int]): Option[PositionedField]
         prepend_append_lengths *: proc (spf: seq[PositionedField]): (int, int)
         ## always determined last
-        field_crc  *: proc (spf: seq[PositionedField], pal: Option[int]): PositionedField
-        use_crc    *: proc (spf: seq[PositionedField], pal: Option[int]): bool
+        field_crc  *: proc (spf: seq[PositionedField], pal: Option[int]): Option[PositionedField]
         crc_after  *: proc (spf: seq[PositionedField], pal: Option[int]): bool
