8016e8f46142c76a379cd7d1bf652bfdad761567
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2025-07-22 19:46:19 -0400

    redo settings object (still unsure)

--- src/cliff/chunk.nim
+++ src/cliff/chunk.nim
@@ -2,15 +2,14 @@ import std/options
 import fields, settings
 
 type
-    CliffChunk* {.inheritable.} = object
-
-    CliffChunkRaw* = object of CliffChunk
+    CliffChunk* {.inheritable.} = ref object
         id   *: PositionedField
         len1 *: PositionedField
         len2 *: Option[PositionedField]
         ver  *: Option[PositionedField]
         crc  *: Option[PositionedField]
 
+    CliffChunkRaw* = ref object of CliffChunk
         ## all data for the chunk is copied into one section, prepend then append then data
         ## pointers in each ByteSection are offset within that
         ##
--- src/cliff/settings.nim
+++ src/cliff/settings.nim
@@ -3,14 +3,15 @@ import fields
 
 type
     CSFieldOrderElement* {.size:sizeof(byte).} = enum
-        order_ID   = 0, ## - identifier of the chunk type
-        order_VER  = 1, ## - version of the chunk type. can be useful for files which are themselves one chunk, that starts with a file wide version
-        order_LEN1 = 2, ## - length 1 of data
-        order_LEN2 = 3, ## - override length of data
-        order_PAL  = 4, ## - calculates lengths of prepend and append
-        order_field_len = 5
+        order_ID   , ## - identifier of the chunk type
+        order_VER  , ## - version of the chunk type. can be useful for files which are themselves one chunk, that starts with a file wide version
+        order_LEN1 , ## - length 1 of data
+        order_LEN2 , ## - override length of data
+        order_PAL  , ## - calculates lengths of prepend and append
+        order_OTHER,
+        order_field_len
 
-    CliffSettings* = object
+    CliffSettingsV1* = object
         ## sets field processing order
         ## ordering must start with ID, LEN1, or VER
         field_order *: array[order_field_len, CSFieldOrderElement]
@@ -22,11 +23,25 @@ type
         ##
         ## once `prepend_append_lengths` is called
         ## all following procs will recieve a value for either the prepend (first three) or append (crc)
-        field_id   *: proc (fields: seq[Option[Field]], pal: Option[int]): PositionedField
-        field_ver  *: proc (fields: seq[Option[Field]], pal: Option[int]): Option[PositionedField]
-        field_len1 *: proc (fields: seq[Option[Field]], pal: Option[int]): PositionedField
-        field_len2 *: proc (fields: seq[Option[Field]], pal: Option[int]): Option[PositionedField]
-        prepend_append_lengths *: proc (fields: seq[Option[Field]]): (int, int)
+        field_id   *: proc (fields: seq[Option[Variant]], pal: Option[int]): PositionedField
+        field_ver  *: proc (fields: seq[Option[Variant]], pal: Option[int]): Option[PositionedField]
+        field_len1 *: proc (fields: seq[Option[Variant]], pal: Option[int]): PositionedField
+        field_len2 *: proc (fields: seq[Option[Variant]], pal: Option[int]): Option[PositionedField]
+        prepend_append_lengths *: proc (fields: seq[Option[Variant]]): (int, int)
         ## always determined last
-        field_crc  *: proc (fields: seq[Option[Field]], pal: Option[int]): Option[PositionedField]
-        crc_after  *: proc (fields: seq[Option[Field]], pal: Option[int]): bool
+        field_crc  *: proc (fields: seq[Option[Variant]], pal: Option[int]): Option[PositionedField]
+        crc_after  *: proc (fields: seq[Option[Variant]], pal: Option[int]): bool
+        fields_other *: seq[proc (fields: seq[Option[Variant]]): Option[PositionedField]]
+
+        ## readlen = ((len * scale1) + offset) * scale2
+        data_len_scale1 *: int
+        data_len_offset *: int
+        data_len_scale2 *: int
+
+type
+    CliffSettingsV2* = object
+        fields_prepend *: seq[proc (fields: seq[Option[Variant]]): Option[PositionedField]]
+        get_id   *: proc (fields: seq[Option[Variant]]): seq[byte]
+        get_lens *: proc (fields: seq[Option[Variant]]): array[3, uint] # prepend, data, append
+        fields_append *: seq[proc (fields: seq[Option[Variant]]): Option[PositionedField]]
+        get_crc  *: proc (fields: seq[Option[Variant]]): seq[byte]
